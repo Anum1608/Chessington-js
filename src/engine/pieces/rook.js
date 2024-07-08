50c688ac5e8e28aeec826159d29d5bdbedc2aafe
anum1608 <riaz.anum@outlook.com>
2024-07-08 15:10:59 +0100

    rook can only move laterally

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,4 +1,6 @@
 import Piece from './piece';
+import GameSettings from '../gameSettings';
+import Square from '../square';
 
 export default class Rook extends Piece {
     constructor(player) {
@@ -6,6 +8,30 @@ export default class Rook extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let location = board.findPiece(this)
+        let movesList = [];
+
+        // add horizontal moves
+        for (let i=0;i<GameSettings.BOARD_SIZE;i++){
+
+            if(i!=location.col){
+                movesList.push(Square.at(location.row,i));
+            }      
+        }
+
+        // add vertical moves
+        for (let i=0;i<GameSettings.BOARD_SIZE;i++){
+
+            if(i!=location.row){
+                movesList.push(Square.at(i,location.col));
+            }      
+        }
+        
+        return movesList;
+
+        // give a list of available moves for rook
+        //can move laterally
+
+        //return new Array(0);
     }
 }
