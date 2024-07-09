bb00862f4e79c3ed2ca1b2eb3cc207c6a5ddea9a
anum1608 <riaz.anum@outlook.com>
2024-07-09 15:14:50 +0100

    rook can take opposing pieces, cannot take the opposing king

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import GameSettings from '../gameSettings';
 import Square from '../square';
+import King from './king';
 
 export default class Rook extends Piece {
     constructor(player) {
@@ -22,36 +23,60 @@ export default class Rook extends Piece {
 
         //right search
         for (let i=location.col+1;i<GameSettings.BOARD_SIZE;i++){
-            if( !board.getPiece(Square.at(location.row,i)) )
-                movesList.push( Square.at(location.row,i) ) 
+            let rightSquare = Square.at(location.row,i)
+            let rightPiece = board.getPiece(rightSquare)
+            if( !rightPiece )
+                movesList.push( rightSquare ) 
+            else if (rightPiece && rightPiece.player !== this.player && !(rightPiece instanceof King)){
+                movesList.push( rightSquare )
+                break
+            }
             else
                 break
         }
 
         // left search
         for (let i=location.col-1; i>-1; i--){
-            if( !board.getPiece(Square.at(location.row,i)) )
-                movesList.push( Square.at(location.row,i) ) 
+            let leftSquare = Square.at(location.row,i)
+            let leftPiece = board.getPiece(leftSquare)
+            if( !leftPiece )
+                movesList.push( leftSquare ) 
+            else if (leftPiece && leftPiece.player !== this.player && !(leftPiece instanceof King)){
+                movesList.push( leftSquare )
+                break
+            }
             else
                 break
         }
 
         //down search
         for (let i=location.row+1;i<GameSettings.BOARD_SIZE;i++){
-            if( !board.getPiece(Square.at(i,location.col)) )
-                movesList.push( Square.at(i,location.col) ) 
+            let downSquare = Square.at(i,location.col)
+            let downPiece = board.getPiece(downSquare)
+            if( !downPiece )
+                movesList.push( downSquare ) 
+            else if (downPiece && downPiece.player !== this.player && !(downPiece instanceof King)){
+                movesList.push( downSquare )
+                break
+            }
             else
                 break
         }
 
         // up search
         for (let i=location.row-1; i>-1; i--){
-            if( !board.getPiece(Square.at(i,location.col)) )
-                movesList.push( Square.at(i,location.col) ) 
+            let upSquare = Square.at(i,location.col)
+            let upPiece = board.getPiece(upSquare)
+            if( !upPiece )
+                movesList.push( upSquare ) 
+            else if (upPiece && upPiece.player !== this.player && !(upPiece instanceof King)){
+                movesList.push( upSquare )
+                break
+            }
             else
                 break
         }
-                
+
         return movesList;
     }
 }
