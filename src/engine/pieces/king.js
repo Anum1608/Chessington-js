56c08e8639478d9a5994612cc02c50ed9d6ca96d
anum1608 <riaz.anum@outlook.com>
2024-07-10 15:56:13 +0100

    trying to code king.js

--- src/engine/pieces/king.js
+++ src/engine/pieces/king.js
@@ -6,6 +6,22 @@ export default class King extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let location = board.findPiece(this)
+        // location.row = 2, location.col = 3
+        let movesList = [];
+
+        for (let i=location.row-1; i<location.row+2; i++ ) {
+            movesList.push(Square.at(i, location.col-1)); // 1, 2, 2, 2, 3, 2
+            movesList.push(Square.at(i, location.col+1)); // 1, 4, 2, 4, 3, 4
+        }
+        // for ()
+
+         
+                
+  /*      Square.at(location.row, location.col+1)
+        Square.at(location.row, location.col-1)
+        Square.at(location.row+1, location.col)*/
+
+        return movesList;
     }
 }
--- tests/engine/pieces/king.spec.js
+++ tests/engine/pieces/king.spec.js
@@ -1,5 +1,29 @@
 import King from '../../../src/engine/pieces/king';
+import Board from '../../../src/engine/board';
+import Square from '../../../src/engine/square';
+import Player from '../../../src/engine/player';
 
 describe('King', () => {
+    let board;
+    beforeEach(() => board = new Board);
 
+    it ('can move only one square in any direction', () => {
+        const king = new King(Player.WHITE);
+        board.setPiece(Square.at(2, 3), king);
+
+        const moves = king.getAvailableMoves(board);
+
+        const expectedMoves = [Square.at(1,2) , Square.at(1,3), Square.at(1,4), Square.at(2,4), Square.at(3,4) ,
+                            Square.at(3,3), Square.at(3,2), Square.at(2,2) ];
+
+        moves.should.deep.include.members(expectedMoves);
+    });
+
+    it ('cannot move through any piece', () => {
+    
+    });
+
+    it ('cannot take any piece', () => {
+    
+    });
 });
